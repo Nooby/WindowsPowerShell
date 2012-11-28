203877fed41b8fa9614f1735a30271d0d348113d
Nooby <Nooby@gmx.eu>
2012-11-28 02:56:29 +0100

    Extended inetp; Added Get-WebFile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,5 +20,49 @@ $ui = (Get-Host).UI.RawUI
 $ui.BackgroundColor = "black"
 $ui.ForegroundColor = "green"
 
-function inetp () { Test-Connection -ComputerName 8.8.8.8 }
+function inetp ($ip = "8.8.8.8") { 
+    if (Test-Connection -ComputerName $ip -quiet) {
+        echo "Up."
+    } else {
+        echo "Down. Testing every 5 Seconds."
+        while(1) { 
+            sleep -sec 5 
+            if (Test-Connection -ComputerName $ip -quiet) {
+                echo "Internet is Up again."
+                return
+            }
+        }
+    }
+}
+
+function Get-WebFile
+{
+    param([String]$Url,[Switch]$UseProxy)
+
+    if ($UseProxy)
+    {
+        $WebProxy = New-Object -Typename System.Net.WebProxy
+        # Anmeldeinformation holen
+        $Cred = Get-Credential
+        $WebProxy.Credentials = $Cred.GetNetworkCredential()
+    }  
+
+    # WebClient-Objekt anlegen
+    $WC = New-Object -TypeName System.Net.WebClient
+
+    try
+    {
+        # Ist ein Proxy im Spiel?
+        if ($WebProxy -ne $null)
+        {
+            $WC.Proxy = $WebProxy
+        }
+        # Datei herunterladen
+        $WC.DownloadString($Url)
+    }
+    catch
+    {
+        Write-Warning "Fehler beim Download ($_)"
+    }
+}
 
